08796048af867fc990c7afd05b9e0a15e3d4bd10
Bangtx <banhtran.hha@gmail.com>
2026-04-23 21:15:15 +0700

    naming conversion

---
+++ js/2026-04-20/ex01.js
@@ -0,0 +1,117 @@
+// javascript - js
+// typescript - ts
+/*
+* js/ts la 1 ngon ngu lap trinh
+* html/css khong phai la 1 ngon ngu lap trinh
+* web - frontend
+*   - jquery
+*   - reactjs
+*   - vuejs
+*   - angular
+* mobile
+*   - react native
+* desktop
+*   - electron js
+* backend
+*   - express js
+*   - nestjs
+* AI
+*
+* ngon ngu lap trinh
+* javascript -> ko chat -> de code -> kho bao tri -> ko ho tro OOP
+*   tu duy lap trinh
+*     - cau truc du lieu & thuat toan
+* typescript -> bo dung type (java/C++), co the lap trinh huong doi tuong -> de bao tri -> code hoi lau
+*   - oop: lap trinh huong doi tuong
+*   - design pattern: 1 so cai quan trong
+* dom -> thao tac vs giao dien - tuong tac vs HTML/CSS
+* ---> du an ket thuc module (*)
+*
+* bien - variable
+*   su dung de luu tru tam thoi 1 gia tri nao day
+*     - luu tru tren ram
+*     - chi co tac dung khi chay tien trinh hien tai (file code hien tai)
+*
+* Khai bao
+*   [var/let/const] <tenBien>
+* Khoi tao 1 bien
+*   [var/let/const] <tenBien> = <value>
+* */
+// let a = 10, b = 200
+//
+// a = 100
+//
+// console.log(a, b)
+//
+// const c = 1000
+// c = 2000
+//
+// console.log(c)
+
+// number
+/*
+const a = 2
+const b = 3
+
+const c = a ** b
+
+console.log(2 ** (1/2))
+console.log(Math.sqrt(2))
+*/
+// string
+/*
+const firstName = '    An'
+const lastName = 'nguyen'
+
+// const fullName = firstName + " " + lastName
+const fullName = `${firstName} ${lastName}`
+
+// console.log(firstName / lastName)
+console.log(fullName.toLowerCase().trim())
+
+console.log('an nguyen'.includes('An N'.toLowerCase()))
+
+console.log('an nguyen'.indexOf('en??'))
+*/
+// bool: true/false --- dai so bool
+// is + adj / has + N
+// and or nor not
+const isStudent = true
+const isBoy = false
+// console.log(isStudent && isBoy)
+// console.log(isStudent || isBoy)
+// console.log(!!isStudent)
+// kong minh la con gai -> false
+// quoc anh la con trai -> true
+// kong minh la con gai va quoc anh la con trai -> false
+// kong minh la con gai hoac quoc anh la con trai -> true
+/*
+* 'va' chi dung khi cac ve cung dung (phep hoi chi dung khi 2 sio dung dung)
+* 'hoac' chi sai khi tat ca deu sai (phep tuiyen chi sai khi 2 so cung sai)'
+*
+* phu dinh cua dung -> sai
+* phu dingh cua sai -> dung
+* */
+// const number = 2
+// console.log(!!!(number**2 >= 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/2026-04-23/ex01.js
@@ -0,0 +1,86 @@
+const a = undefined
+// const b = 3
+
+// a chia b lay phan du
+/*
+* 2 / 3 = 0 du 2
+* */
+
+// const number = 11
+//
+// const remainder = number % 3
+//
+// console.log(`${number} divided by 3 remainder ${remainder}`)
+
+// if (number % 3 === 0) {
+//   console.log(`${number} divided by 3 remainder 0`)
+// }
+// else if (number % 3 === 1) {
+//   console.log(`${number} divided by 3 remainder 1`)
+// }
+// else {
+//   console.log(`${number} divided by 3 remainder 2`)
+// }
+
+// console.log(undefined || null)
+
+/*
+* 70
+*   - 50 - 1.678
+*   - 20 - 1.734
+* => 50 * 1.678 + 20 * 1.734
+*
+* 120
+*   - 50 - 1.678
+*   - 50 - 1.734
+*   - 20 - 2.014
+* => 50 * 1.678 + 50 * 1.734 + 20 * 2.014
+* */
+
+/*
+* ham - function
+* f(x) = x + 1
+* x = 1 -> f(x) = 2
+*
+* f(x) = x**2 + 2x + 1
+* x = 1 -> f(x) = 4
+*
+* f(x, y) = x + 2y
+* (1, 2) => f(1, 2) = 1 + 2*2
+* => x, y: tham so / parameters (params)
+* => 1, 2: doi so / arguments (args)
+*
+* f() = 1
+*
+* */
+
+// function doSomething() {
+//   console.log('hj ngai qua')
+// }
+//
+// const y = doSomething()
+//
+// console.log(y)
+
+// function f() {
+//   return 1
+// }
+// const y = f()
+// console.log(y)
+
+
+// function f(x, y) {
+//   return x + 2 * y
+// }
+//
+// console.log(f(1, 2))
+
+// function f(x) {
+//   return x + 1
+// }
+//
+// const y = f(1)
+//
+// console.log(y)
+
+// biet ham check tinh chan/le cua 1 so
