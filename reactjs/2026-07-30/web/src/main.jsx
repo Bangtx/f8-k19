bf17839526b30569740d059e6a374895b8643aff
bangtx <bangtran.hha@gmail.com>
2026-07-30 20:24:27 +0700

    react router

--- reactjs/2026-07-30/web/src/main.jsx
+++ reactjs/2026-07-30/web/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import HomePage from "./pages/index.jsx";
+import Products from "./pages/Products/index.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+  {
+    path: "/products",
+    element: <Products/>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
---
+++ reactjs/2026-07-30/web/src/pages/Products/index.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import api from '../../plugins/axios'
+
+function Products() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Products
